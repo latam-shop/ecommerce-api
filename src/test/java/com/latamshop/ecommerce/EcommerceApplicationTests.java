86ad96214b18af5709e5233cefff2e6a048a8e2a
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2026-03-15 13:04:22 -0300

    ci(github): inject temporary application.properties with H2 for testing

--- src/test/java/com/latamshop/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/latamshop/ecommerce/EcommerceApplicationTests.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class EcommerceApplicationTests {
 
   @Autowired private MockMvc mockMvc;
