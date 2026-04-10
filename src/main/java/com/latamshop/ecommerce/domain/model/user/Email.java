80c5b1159d4721fbce74301071ca37820cb6fd0c
Diego Hoyos <dfhoyos29@gmail.com>
2026-04-09 22:52:41 -0500

    enhance

--- src/main/java/com/latamshop/ecommerce/domain/model/user/Email.java
+++ src/main/java/com/latamshop/ecommerce/domain/model/user/Email.java
@@ -3,16 +3,15 @@ package com.latamshop.ecommerce.domain.model.user;
 import java.util.regex.Pattern;
 
 public record Email(String value) {
-    private static final Pattern EMAIL_PATTERN =
-            Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+  private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
 
-    public Email {
-        if (value == null || value.isBlank()) {
-            throw new IllegalArgumentException("Email cannot be empty");
-        }
-
-        if (!EMAIL_PATTERN.matcher(value).matches()) {
-            throw new IllegalArgumentException("Invalid email");
-        }
+  public Email {
+    if (value == null || value.isBlank()) {
+      throw new IllegalArgumentException("Email cannot be empty");
     }
+
+    if (!EMAIL_PATTERN.matcher(value).matches()) {
+      throw new IllegalArgumentException("Invalid email");
+    }
+  }
 }
--- src/test/java/com/latamshop/ecommerce/domain/model/EmailTest.java
+++ src/test/java/com/latamshop/ecommerce/domain/model/EmailTest.java
@@ -1,25 +1,25 @@
 package com.latamshop.ecommerce.domain.model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import com.latamshop.ecommerce.domain.model.user.Email;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-
 public class EmailTest {
-    @ParameterizedTest
-    @ValueSource(strings = { "invalidemail.com" })
-    void invalidFormat(String invalidEmail){
-        assertThatThrownBy(() ->
-                new Email(invalidEmail)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Invalid email");
-    }
+  @ParameterizedTest
+  @ValueSource(strings = {"invalidemail.com"})
+  void invalidFormat(String invalidEmail) {
+    assertThatThrownBy(() -> new Email(invalidEmail))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Invalid email");
+  }
 
-    @ParameterizedTest
-    @ValueSource(strings = { "" })
-    void emptyValue(String emptyValue){
-        assertThatThrownBy(() ->
-                new Email(emptyValue)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Email cannot be empty");
-    }
+  @ParameterizedTest
+  @ValueSource(strings = {""})
+  void emptyValue(String emptyValue) {
+    assertThatThrownBy(() -> new Email(emptyValue))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Email cannot be empty");
+  }
 }
