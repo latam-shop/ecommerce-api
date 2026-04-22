f883259844f073b368fe0e3c67aa1b741bbe7f2f
Diego Hoyos <dfhoyos29@gmail.com>
2026-04-21 21:41:43 -0500

    entity address

--- src/main/java/com/latamshop/ecommerce/infrastructure/adapter/DnsEmailValidatorAdapter.java
+++ src/main/java/com/latamshop/ecommerce/infrastructure/adapter/DnsEmailValidatorAdapter.java
@@ -1,5 +1,4 @@
 package com.latamshop.ecommerce.infrastructure.adapter;
-
 import com.latamshop.ecommerce.domain.port.EmailValidatorPort;
 import java.util.Hashtable;
 import java.util.List;
@@ -10,7 +9,30 @@ import org.springframework.stereotype.Component;
 public class DnsEmailValidatorAdapter implements EmailValidatorPort {
 
   private static final List<String> BLACKLIST =
-      List.of("yopmail.com", "tempmail.org", "10minutemail.com");
+      List.of("yopmail.com", "tempmail.org", "10minutemail.com",
+              "yuoia.com",
+              "uzxia.com",
+              "jontra.com",
+              "gienig.com",
+              "cwtaa.com",
+              "dusyum.com",
+              "baddomain.com",
+              "nezid.com",
+              "omeie.com",
+              "zbock.com",
+              "zslsz.com",
+              "cazlq.com",
+              "armyspy.com",
+              "cuvox.de",
+              "einrot.com",
+              "fleckens.hu",
+              "gustr.com",
+              "jourrapide.com",
+              "rhyta.com",
+              "superrito.com",
+              "teleworm.us",
+              "holis.com",
+              "example.com");
 
   @Override
   public boolean isReal(String email) {
